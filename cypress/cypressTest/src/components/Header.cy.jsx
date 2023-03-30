1b73ee6c602a2985c3cfe89e4407dfdb31faaaed
Vicente Viera <plantedcoot706@gmail.com>
2023-03-29 20:14:13 -0600

    README improved

--- cypress/cypressTest/src/components/Header.cy.jsx
+++ cypress/cypressTest/src/components/Header.cy.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Header from './Header'
 
 describe('<Header />', () => {
-  it('renders', () => {
-    cy.mount(<Header />);
-    cy.get("[data-cy=header]").should("have.text", "Hola Crayola");
-  });
-})
+	it('renders', () => {
+		cy.mount(<Header />);
+		cy.get("[data-cy=header]").should("have.text", "Hola Crayola");
+	});
+});
