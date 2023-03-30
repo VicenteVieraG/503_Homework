b11b74b8fdf5cad9cf12834e66f80b9174c332d1
Vicente Viera <plantedcoot706@gmail.com>
2023-03-29 18:55:03 -0600

    First Test Added

---
+++ cypress/cypressTest/src/components/Header.cy.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from './Header'
+
+describe('<Header />', () => {
+  it('renders', () => {
+    cy.mount(<Header />);
+    cy.get("[data-cy=header]").should("have.text", "Hola Crayola");
+  });
+})
---
+++ cypress/cypressTest/src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <h1 data-cy="header">Hola Crayola</h1>
+
+export default Header;
